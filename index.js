a5b1ac7b13e9273315d0f4eb06d9f91ff4c24533
jose montoya <jose_montoya_guzman@hotmail.com>
2018-08-21 01:07:48 -0500

    commit de consultas

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const app = express();
+const express_graphql = require("express-graphql");
+const { buildSchema } = require("graphql");
+const { courses } = require("./data.json");
+
+/*const schema = buildSchema(`
+  type Query {
+    message: String
+  }
+`);*/
+const schema = buildSchema(`
+  type Query {
+    getCourseById(id: Int!): Course
+    getJavascriptCourses(topic: String): [Course] 
+  }
+
+  type Course {
+    id: Int
+    title: String
+    description: String
+    author: String
+    topic: String
+    url: String
+  }
+`);
+
+let getCourseById = args => {
+  let id = args.id;
+  return courses.filter(course => {
+    if (course.id == id) {
+      return course;
+    }
+  })[0];
+};
+
+let getJavascriptCourses = args => {
+  if (args.topic) {
+    return courses.filter(course => course.topic === args.topic);
+  } else {
+    return courses;
+  }
+};
+
+/*const root = {
+  message: () => "Hello world!"
+};*/
+
+const root = {
+  getCourseById: getCourseById,
+  getJavascriptCourses: getJavascriptCourses
+};
+
+app.use(
+  "/graphql",
+  express_graphql({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+  })
+);
+
+app.listen(3000, () => console.log("Server init on port 3000..."));
